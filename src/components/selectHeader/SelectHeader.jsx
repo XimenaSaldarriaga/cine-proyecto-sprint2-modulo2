a2db07b0816e518481a736f8c7771456cd1820af
ximena saldarriaga <xime-2208@hotmail.com>
2023-08-08 17:41:25 -0500

    add button logout

--- src/components/selectHeader/SelectHeader.jsx
+++ src/components/selectHeader/SelectHeader.jsx
@@ -5,18 +5,24 @@ import SearchMovie from '../searchMovie/SearchMovie';
 import { URL_USERS } from '../../services/data';
 import { useNavigate } from 'react-router-dom';
 import location from '../../images/location.png';
+import { IoIosCloseCircleOutline } from 'react-icons/io';
+
 
 const SelectHeader = () => {
   const [showLoginForm, setShowLoginForm] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);  // Estado para controlar el inicio de sesión
   const [rememberMe, setRememberMe] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [users, setUsers] = useState([]);
-  const [loggedInUser, setLoggedInUser] = useState(null);  // Usuario que ha iniciado sesión
+  const [loggedInUser, setLoggedInUser] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem('loggedInUser'));
+    if (storedUser) {
+      setLoggedInUser(storedUser);
+    }
+
     axios.get(URL_USERS)
       .then(response => {
         setUsers(response.data);
@@ -34,99 +40,45 @@ const SelectHeader = () => {
     setShowLoginForm(false);
   };
 
-  const handleRememberMeChange = event => {
+  const handleRememberMeChange = (event) => {
     setRememberMe(event.target.checked);
   };
 
-  const handleEmailChange = event => {
+  const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
 
-  const handlePasswordChange = event => {
+  const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
-  const handleLoginFormSubmit = event => {
+  const handleLoginFormSubmit = (event) => {
     event.preventDefault();
 
-    const user = users.find(user => user.email === email && user.password === password);
+    const user = users.find((user) => user.email === email && user.password === password);
 
     if (user) {
-      setIsLoggedIn(true);
       setLoggedInUser(user);
       setShowLoginForm(false);
+      localStorage.setItem('loggedInUser', JSON.stringify(user));
     } else {
       console.log('Invalid login');
     }
   };
 
+  const handleLogout = () => {
+    setLoggedInUser(null);
+    localStorage.removeItem('loggedInUser');
+  };
+
   return (
     <div className='selectHeader'>
-      {showLoginForm && (
-        <div className='selectHeader__loginFormOverlay'>
-          <div className='selectHeader__loginFormContainer'>
-            <button className='selectHeader__closeButton' onClick={closeLoginForm}>
-              <span>X</span>
-            </button>
-            <div className='selectHeader__loginForm'>
-              <h1>Bienvenido</h1>
-              <h2>Inicia Sesión</h2>
-              <form className='selectHeader__form' onSubmit={handleLoginFormSubmit}>
-                  <div>
-                    <label className='selectHeader__labelform' htmlFor='email'>
-                      Correo electrónico
-                    </label>
-                    <input
-                      className='selectHeader__inputform'
-                      type='email'
-                      id='email'
-                      name='email'
-                      value={email}
-                      onChange={handleEmailChange}
-                      required
-                    />
-                  </div>
-                  <div>
-                    <label className='selectHeader__labelform' htmlFor='password'>
-                      Contraseña
-                    </label>
-                    <input
-                      className='selectHeader__inputform'
-                      type='password'
-                      id='password'
-                      name='password'
-                      value={password}
-                      onChange={handlePasswordChange}
-                      required
-                    />
-                  </div>
-                  <div className='selectHeader__rememberMe'>
-                    <input
-                      className='selectHeader__checkbox'
-                      type='checkbox'
-                      id='rememberMe'
-                      name='rememberMe'
-                      checked={rememberMe}
-                      onChange={handleRememberMeChange}
-                    />
-                    <label className='selectHeader__labelform' htmlFor='rememberMe'>
-                      Recordarme
-                    </label>
-                  </div>
-                  <button className='selectHeader__login' type='submit'>
-                    Iniciar sesión
-                  </button>
-                </form>
-            </div>
-          </div>
-        </div>
-      )}
-      {!isLoggedIn ? (
+      {loggedInUser ? (
         <div className='selectHeader__info'>
           <SearchMovie />
           <div className='selectHeader__loc'>
             <span className='selectHeader__city'>Medellín</span>
-            <img className='selectHeader__location' src={location} alt='' />
+            <img className='selectHeader__location' src={location} alt="" />
           </div>
           <img
             className='selectHeader__icon'
@@ -134,25 +86,91 @@ const SelectHeader = () => {
             alt=''
             onClick={handleImageClick}
           />
+          <div className='selectHeader__profileSection'>
+            <span className='selectHeader__profileName'>{loggedInUser.name}</span>
+            <IoIosCloseCircleOutline className='selectHeader__profileButton' onClick={handleLogout}/>
+          </div>
         </div>
       ) : (
-        <div className='selectHeader__loggedInInfo'>
-          <p className='selectHeader__loggedInUser'>{loggedInUser.name}</p>
-          <span className='selectHeader__view'>Ver Perfil</span>
+        <div>
+          {showLoginForm && (
+            <div className='selectHeader__loginFormOverlay'>
+              <div className='selectHeader__loginFormOverlay'>
+                <div className='selectHeader__loginFormContainer'>
+                  <button className='selectHeader__closeButton' onClick={closeLoginForm}>
+                    <span>X</span>
+                  </button>
+                  <div className='selectHeader__loginForm'>
+                    <h1>Bienvenido</h1>
+                    <h2>Inicia Sesión</h2>
+                    <form className='selectHeader__form' onSubmit={handleLoginFormSubmit}>
+                      <div>
+                        <label className='selectHeader__labelform' htmlFor='email'>
+                          Correo electrónico
+                        </label>
+                        <input
+                          className='selectHeader__inputform'
+                          type='email'
+                          id='email'
+                          name='email'
+                          value={email}
+                          onChange={handleEmailChange}
+                          required
+                        />
+                      </div>
+                      <div>
+                        <label className='selectHeader__labelform' htmlFor='password'>
+                          Contraseña
+                        </label>
+                        <input
+                          className='selectHeader__inputform'
+                          type='password'
+                          id='password'
+                          name='password'
+                          value={password}
+                          onChange={handlePasswordChange}
+                          required
+                        />
+                      </div>
+                      <div className='selectHeader__rememberMe'>
+                        <input
+                          className='selectHeader__checkbox'
+                          type='checkbox'
+                          id='rememberMe'
+                          name='rememberMe'
+                          checked={rememberMe}
+                          onChange={handleRememberMeChange}
+                        />
+                        <label className='selectHeader__labelform' htmlFor='rememberMe'>
+                          Recordarme
+                        </label>
+                      </div>
+                      <button className='selectHeader__login' type='submit'>
+                        Iniciar sesión
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+          <div className='selectHeader__info'>
+            <SearchMovie />
+            <div className='selectHeader__loc'>
+              <span className='selectHeader__city'>Medellín</span>
+              <img className='selectHeader__location' src={location} alt="" />
+            </div>
+            <img
+              className='selectHeader__icon'
+              src='https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png'
+              alt=''
+              onClick={handleImageClick}
+            />
+          </div>
         </div>
       )}
     </div>
   );
-      }
+};
 
 export default SelectHeader;
-
-
-
-
-
-
-
-
-
-
