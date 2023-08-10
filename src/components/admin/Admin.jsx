e88058e7fbea313081e21f02d5dac15833cc1344
ximena saldarriaga <xime-2208@hotmail.com>
2023-08-10 16:40:55 -0500

    add selectedMovie to admin

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -8,7 +8,7 @@ import { getDataMovies } from '../../services/data';
 import { useParams } from 'react-router-dom';
 import { API_URL, API_KEY, URL_IMAGE, URL_THEATERS } from '../../services/data';
 
-const Admin = ({data, navigate}) => {
+const Admin = ({ data }) => {
 
   const { id } = useParams();
   const [theaters, setTheaters] = useState([]);
@@ -39,22 +39,6 @@ const Admin = ({data, navigate}) => {
   console.log(id)
   console.log(selectedMovie)
 
-  // const fetchMovie = async (id) => {
-  //   const { data } = await axios.get(`${API_URL}/movie/${id}`, {
-  //     params: {
-  //       api_key: API_KEY,
-  //       append_to_response: 'videos',
-  //     },
-  //   });
-
-  //   if (data.videos && data.videos.results) {
-  //     const trailer = data.videos.results.find((vid) => vid.name === 'Official Trailer');
-  //     setTrailer(trailer ? trailer : data.videos.results[0]);
-  //     setMovie(data);
-  //   }
-  // };
-
-
   const getTheatersData = async () => {
     try {
       const { data } = await axios.get(URL_THEATERS);
@@ -74,23 +58,32 @@ const Admin = ({data, navigate}) => {
     setSelectedTheater(selectedTheater === theater ? null : theater);
   };
 
-  
 
   return (
     <div>
-      <HeaderNav loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser}/>
-      <div className='admin'>
-        <div></div>
-        <div className='admin__main'>
-          <div className='admin__info'>
-            <p>{selectedMovie.overview}</p>
-            <p>Título Original</p>
-            <p>{selectedMovie.original_title}</p>
-            <p>Pais de origen</p>
-            <p>Director</p>
-            <p>Actores</p>
-            <p>Lenguaje</p>
+      <HeaderNav loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
+      {selectedMovie ? (
+          <div className='admin__video'>
+            <img className='admin__img' src={`${URL_IMAGE}${selectedMovie.poster_path}`} alt='' />
           </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      <div className='admin'>
+        <div className='admin__main'>
+          {selectedMovie ? (
+            <div className='admin__info'>
+              <p>{selectedMovie.overview}</p>
+              <p>Título Original</p>
+              <p>{selectedMovie.original_title}</p>
+              <p>Pais de origen</p>
+              <p>Director</p>
+              <p>Actores</p>
+              <p>Lenguaje</p>
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
           <div className='admin__cinemas'>
             <div className='admin__dates'>
               {theaters[0]?.dates.map((date, index) => {
